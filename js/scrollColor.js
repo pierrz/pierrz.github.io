a965ee172a3abbccee6234e7bb9b84fee1b9fdce
pierrz <pierrz@tuta.io>
2020-04-09 00:30:04 +0200

    1st commit
    Straight copy from the server

---
+++ js/scrollColor.js
@@ -0,0 +1,36 @@
+/**
+ * scrollColor.js
+ * http://www.pierrz.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2017, Pierre-Edouard Barrault
+ * http://www.pierrz.com
+ */
+			//SCROLL COLOR
+          // $(function() {
+          //       var gap_bio = $( "#bio .row" );
+          //       $( gap_bio ).mouseover(function() {
+          //                   $( "#logo > a" ).css({ "color": "red" });
+          //           });
+
+          //           //Mouse Out cell
+          //           $( gap_bio ).mouseout(function() {
+          //                 $( "#logo > a" ).css({ "color": "white" });
+          //           });
+          //         // inView( gap_bio )
+          //         //       .on('enter', el => {
+          //         //           ('#logo').style.color = 'red';
+          //         //       })
+          //         //       .on('exit', el => {
+          //         //           ('#logo').style.color = 'white';
+          //         //       });
+          // });
+          
+          
+          
+          // var scrollPos = $( "#logo" ).scrollTop,
+          //     dynColor = 255 - 6 / scrollPos,
+          //     dynRGBA = "rgba(" + dynColor + "," + dynColor + "," + dynColor + ", 1)";
+          // $( "#logo" ).css({ "color": dynRGBA });
